3409efba326984f0f0ba6c6672a602c321615c0d
clpowe <clpowe@gmail.com>
2024-11-29 17:55:22 -0500

    50mins into tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import rooms from '@/data/rooms.json';
+import Heading from '@/components/Heading';
+import RoomCard from '@/components/RoomCard';
 
 export default function Home() {
   return (
     <>
-      <h1>Bookit App</h1>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No rooms available at this time.</p>
+      )}
     </>
+
   );
 
 }
